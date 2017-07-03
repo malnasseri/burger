9503fc63d2910c8a3347d3e63524956af1949978
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2017-07-03 15:45:18 -0400

    Added code to connect to mysql db

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,28 @@
 //requiring mysql package
 var mysql = require("mysql");
 // Create the MySQL connection object
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-// Make the connection to MySQL
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection; 
+
+	if(process.env.JawsDB_URL) {
+
+		connection = mysql.createConnection(process.env.JawsDB_URL);
+   }else {
+   		connection = mysql.createConnection({
+
+   			host: "localhost",
+            user: "root",
+            password: "",
+            database: "burgers_db"
+   		})
+
+
+   		
+   }
+
+
+
+
+
+connection.connect();
 // Exporting connection
 module.exports = connection;
